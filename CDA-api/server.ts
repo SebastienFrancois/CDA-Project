b80fa345d8085ada3c4bb188e1a70d6b17b8c2f5
SebastienFrancois <sebastien.devlopweb@gmail.com>
2022-04-15 12:06:19 +0200

    Api[DB] connect to cluster mongoDB Atlas

--- CDA-api/server.ts
+++ CDA-api/server.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config'
 import express, { Request, Response, NextFunction } from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
--- CDA-api/utils/connect.ts
+++ CDA-api/utils/connect.ts
@@ -1,12 +1,13 @@
 import mongoose from 'mongoose'
+require('dotenv').config()
 
-const url = ''
+const url = `mongodb+srv://avengers:${process.env.DB_PASS}@simpleplan.ye9gc.mongodb.net/simpleplan?retryWrites=true&w=majority`
 
 export default async function connect() {
   try {
-    // await mongoose.connect(url, {
-    //   autoIndex: true,
-    // })
+    await mongoose.connect(url, {
+      autoIndex: true,
+    })
     console.log('Connected to database')
   } catch (err) {
     console.log(err)
