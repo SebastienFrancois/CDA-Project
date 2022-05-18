a7b8dc2363f217cdaaff77c39bdb13af87efa92c
SebastienFrancois <sebastien.devlopweb@gmail.com>
2022-05-18 11:22:08 +0200

    Api - CRUD instance of Project

--- CDA-api/src/graphql/params.ts
+++ CDA-api/src/graphql/params.ts
@@ -12,29 +12,47 @@ const typeDefs = gql`
         updatedAt: String
     }
     type Query {
-        getProjects: [Project]
+        getProjects: [Project],
+        getProject(id: ID!): Project
     }
     type Mutation {
         addProject(name: String!, description: String, status: String,dueDate: String!): Project
-        deleteProject(id: ID!): Boolean!
-        updateProject(id: ID!, name: String!, description: String, status: String,dueDate: String!): Project!
+        deleteProject(id: ID!): String!
+        updateProject(id: ID!, name: String, description: String, status: String,dueDate: String): Project
     }
 `
 const resolvers = {
     Query: {
         getProjects: async () => await ProjectModel.find({}),
+        getProject: async (_:ParentNode, args: {id: String}) => await ProjectModel.findById({_id: args.id}) 
     },
     Mutation: {
         addProject: async ( _ :ParentNode, args: IProject ) => {
             const newProject = await ProjectModel.create({
-            name: args.name,
-            description: args.description,
-            status: args.status,
-            dueDate: args.dueDate
-        })
-        newProject.save()
-        return newProject
-    }
+                name: args.name,
+                description: args.description,
+                status: args.status,
+                dueDate: args.dueDate
+            })
+            newProject.save()
+            return newProject
+        },
+        deleteProject : async (_:ParentNode, args: {id: String}) => {
+            try {
+                await ProjectModel.findOneAndDelete({_id: args.id})
+                return JSON.stringify({message:`Instance "${args.id}" has been deleted successfully !`})
+            } catch (error) {
+                return JSON.stringify({message:` Instance "${args.id}" wasn't deleted !`})
+            }
+        },
+        updateProject: async (_:ParentNode, args: {id: String}) => {
+            try {
+                const newProject = await ProjectModel.findByIdAndUpdate({_id: args.id}, args, {new: true});
+                return newProject
+            } catch (error) {
+                return JSON.stringify(`Instance "${args.id}" wasn't updated !`)
+            }
+        },
     }
 }
 
