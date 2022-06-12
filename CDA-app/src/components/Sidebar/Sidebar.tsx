986bc5a3b5ff1d2decd1191484c7664fd9410707
SebastienFrancois <sebastien.devlopweb@gmail.com>
2022-06-12 17:42:10 +0200

    APP - sidebar white links

--- CDA-app/src/components/Sidebar/Sidebar.tsx
+++ CDA-app/src/components/Sidebar/Sidebar.tsx
@@ -1,46 +1,67 @@
 import React, { FC } from 'react';
 import './Sidebar.scss';
 
-interface SidebarProps {}
+interface SidebarProps {
+  projects: {
+    getProjects: {
+      _id: string;
+      name: string;
+      description: string;
+      status: string;
+      dueDate: string;
+      createdAt: string;
+      updatedAt: string;
+    }[];
+  };
+}
 
 const Sidebar: FC<SidebarProps> = (props) => {
   console.log(props);
   return (
-  <nav className="Sidebar bg-primary h-full flex flex-col justify-between p-3">
-    <div>
-      <img src="" alt="LOGO" />
-      <hr className="border-0 h-0.5 max-h-px bg-secondary"/>
+    <nav className="Sidebar bg-primary h-full flex flex-col justify-between p-3">
       <div>
-        <h2 className="text-secondary text-2xl font-bold my-4">
-          <a href="#">Dashboard</a>
-        </h2>
+        <img src="" alt="LOGO" />
+        <hr className="border-0 h-0.5 max-h-px bg-secondary" />
+        <div>
+          <h2 className="text-secondary text-2xl font-bold my-4">
+            <a href="#">Dashboard</a>
+          </h2>
+        </div>
+        <hr className="border-0 h-0.5 max-h-px bg-secondary" />
+        <div className="mb-5">
+          <h2 className="text-secondary text-2xl font-bold my-4">Projects</h2>
+          <ul>
+            {props.projects.getProjects.map((project: any, key: any) => {
+              return (
+                <li className="my-2" key={project._id}>
+                  <a className="text-white text-lg" href="#">
+                    {project.name}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <hr className="border-0 h-0.5 max-h-px bg-secondary" />
+        <div>
+          <h2 className="text-secondary text-2xl font-bold my-4">
+            <a href="#">Users</a>
+          </h2>
+        </div>
+        <hr className="border-0 h-0.5 max-h-px bg-secondary" />
+        <div>
+          <h2 className="text-secondary text-2xl font-bold my-4">
+            <a href="#">Admnistration</a>
+          </h2>
+        </div>
       </div>
-      <hr className="border-0 h-0.5 max-h-px bg-secondary"/>
       <div>
-        <h2 className="text-secondary text-2xl font-bold my-4">Projects</h2>
-        <ul>
-          {props.projects.getProjects.map((project: any, key: any) => {
-            return (<li key={project._id}><a href="#">{project.name}</a></li>)
-          })}
-        </ul>
+        <a href="#" className="text-white">
+          Logout
+        </a>
       </div>
-      <hr className="border-0 h-0.5 max-h-px bg-secondary"/>
-      <div>
-        <h2 className="text-secondary text-2xl font-bold my-4">
-          <a href="#">Users</a>
-        </h2>
-      </div>
-      <hr className="border-0 h-0.5 max-h-px bg-secondary"/>
-      <div>
-        <h2 className="text-secondary text-2xl font-bold my-4">
-          <a href="#">Admnistration</a>
-        </h2>
-      </div>
-    </div>
-    <div>
-      <a href="#" className="text-white">Logout</a>
-    </div>
-  </nav>
-)};
+    </nav>
+  );
+};
 
 export default Sidebar;
