ca52fe4c2caa2b03ca3f69a1ad3e125d93fb412c
SebastienFrancois <sebastien.devlopweb@gmail.com>
2022-06-15 16:38:32 +0200

    APP - addProject DONE

--- CDA-app/src/components/ProjectForm/ProjectForm.tsx
+++ CDA-app/src/components/ProjectForm/ProjectForm.tsx
@@ -2,7 +2,7 @@
 import React, { FC, useState } from 'react';
 // eslint-disable-next-line import/named
 import { SubmitHandler, useForm } from 'react-hook-form';
-import { useMutation } from '@apollo/client';
+import { useApolloClient, useMutation } from '@apollo/client';
 import HeaderMain from 'components/HeaderMain/HeaderMain';
 import './ProjectForm.scss';
 import InputBasic from 'components/InputBasic/InputBasic';
@@ -21,15 +21,12 @@ const ProjectForm: FC<ProjectFormProps> = () => {
   const { register, handleSubmit } = useForm<FormValues>();
   const navigate = useNavigate();
   const [gError, setgError] = useState<string>('');
-  const [addProject, { data, loading, error }] = useMutation(PROJECTS.add);
+  const [addProject, { error }] = useMutation(PROJECTS.add);
   if (error) setgError(error.message);
 
   const onSubmit: SubmitHandler<FormValues> = (formState) => {
-    addProject({ variables: formState })
-      .then((d) => {
-        if (d) {
-          alert(JSON.stringify(d));
-        }
+    addProject({ variables: formState, refetchQueries: 'active' })
+      .then(() => {
         navigate('/');
       })
       .catch((err) => setgError(err.message));
@@ -39,8 +36,8 @@ const ProjectForm: FC<ProjectFormProps> = () => {
     <div className="w-10/12 p-6 max-h-screen flex flex-col justify-start items-start">
       <HeaderMain />
       <h1 className=" text-3xl my-4 text-primary font-medium flex">Project informations</h1>
-      {gError && <span className="w-full text-left text-red-600">{gError}</span>}
-      <form className="w-full mb-6" onSubmit={handleSubmit(onSubmit)}>
+      {gError && <span className="w-full text-left text-red-600 my-2 ">{gError}</span>}
+      <form className="w-full mb-3" onSubmit={handleSubmit(onSubmit)}>
         <section id="left" className="w-3/6 pr-4 space-y-4">
           <InputBasic
             type="text"
@@ -61,7 +58,7 @@ const ProjectForm: FC<ProjectFormProps> = () => {
           <InputBasic type="date" label="due date" name="dueDate" required register={register} />
         </section>
         <section id="right"></section>
-        <span className="w-full text-center">
+        <span className="w-full h-auto m-8 flex justify-center align-middle">
           <BasicButton content="Create" type="submit" onClick={() => null} />
         </span>
       </form>
