b5922cd097715fe1ea9348010ac786e8733656d1
SebastienFrancois <sebastien.devlopweb@gmail.com>
2022-07-21 16:18:17 +0200

    Set authorization headers

--- CDA-app/src/App.tsx
+++ CDA-app/src/App.tsx
@@ -2,15 +2,31 @@ import React from 'react';
 import { AuthContext } from 'contexts/AuthContext';
 import AppAuthenticated from 'views/AppAuthenticated/AppAuthenticated';
 import AppUnauthenticated from 'views/AppUnauthenticated/AppUnauthenticated';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-// import { useTranslation } from 'react-i18next';
-const client = new ApolloClient({
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
   uri: 'http://localhost:5000/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('access-token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? token : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
 function App() {
-  // const [t] = useTranslation();
   const { isLogged } = React.useContext(AuthContext);
 
   return (
