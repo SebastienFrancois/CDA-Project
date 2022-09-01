de889969c517f8cdd931db21e8413d99014f1b78
Morgane Lailler <morgane.lailler.pro@gmail.com>
2022-09-01 15:19:29 +0200

    add query get one project

--- CDA-app/src/components/Project/Project.tsx
+++ CDA-app/src/components/Project/Project.tsx
@@ -1,16 +1,21 @@
 import React, { FC } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { PROJECTS } from 'api/query';
 import './Project.scss';
 
 interface ProjectProps {}
 export const Project: FC<ProjectProps> = () => {
+  const {id} = useParams();
 
-  const search = useLocation().search;
-  const id = new URLSearchParams(search).get('id');
+  const { loading, error, data } = useQuery(PROJECTS.getOne, {
+    variables: { getProjectId: id }
+  });
 
+  console.log(data)
   return (
     <div >
-        <h1>Coucou {id}</h1>
+        <h1>Coucou { id }</h1>
     </div>
   );
 };
