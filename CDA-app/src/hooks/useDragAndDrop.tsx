7d341677dac186b77961b9fbc970fb716f8aedac
SebastienFrancois <sebastien.devlopweb@gmail.com>
2022-10-04 10:09:04 +0200

    update useDragAndDrop

--- CDA-app/src/hooks/useDragAndDrop.tsx
+++ CDA-app/src/hooks/useDragAndDrop.tsx
@@ -11,10 +11,13 @@ export const useDragAndDrop = (initialState: ITask[] = []) => {
   const handleDragging = (dragging: boolean) => setIsDragging(dragging);
 
   const handleUpdateList = (id: string, status: TaskStatus) => {
-    const card = listItems.find((item) => item._id === id);
+    let card = listItems.find((item) => item._id === id);
 
     if (card && card.status !== status) {
-      card.status = status;
+      // Create a deep copy from card because card is not "writable" on line 14
+      let newCard = { ...card };
+      newCard.status = status;
+      card = newCard;
       updateTask({
         variables: { updateTaskId: card._id, status: card.status },
         refetchQueries: 'active',
@@ -22,7 +25,7 @@ export const useDragAndDrop = (initialState: ITask[] = []) => {
       // We call the setListItems to update the status, placing:
       // The card with its updated status property.
       // A new array, filtering the items to remove the card we are updating and avoid duplicating the information.
-      setListItems((prev) => [card, ...prev.filter((item) => item._id !== id)]);
+      setListItems((prev) => [card, ...prev.filter((item) => item._id !== id)] as ITask[]);
     }
   };
 
